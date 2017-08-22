d6e253646593f28bef26884b41f04f2b7c0e6db4
caoxiao9 <caoxiao90@163.com>
2017-08-22 14:56:14 +0800

    通过清单文件提供key

--- MaraRunServiceDemo/app/src/main/java/com/mararun/runservice/sample/MainActivity.java
+++ MaraRunServiceDemo/app/src/main/java/com/mararun/runservice/sample/MainActivity.java
@@ -1,19 +1,13 @@
 package com.mararun.runservice.sample;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
 import com.mararun.runservice.engine.MaraTrackerManager;
 import com.mararun.runservice.util.MaraLogger;
-
-import java.util.concurrent.TimeUnit;
-
-import rx.Observable;
-import rx.android.schedulers.AndroidSchedulers;
-import rx.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -41,7 +35,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private void prepare() {
         // 在使用引擎之前，需要进行初始化
         // DEMO中放在activity内进行此操作。实际使用中，建议放置在Application内，进行全局控制
-        TextView tvInfo = (TextView)findViewById(R.id.tv_info);
+        TextView tvInfo = (TextView) findViewById(R.id.tv_info);
         String keyString = "";
         keyString = keyString.concat("App MaraTrack Key: ")
                 .concat(Constants.TRACK_ENGINE_KEY)
@@ -50,7 +44,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         tvInfo.append(getString(R.string.preparing_engine));
         MaraTrackerManager.getInstance().init(
                 this,
-                Constants.TRACK_ENGINE_KEY,
                 (result, reason) -> {
                     MaraLogger.i("track engine auth result:" + result);
                     String infoString = "";
